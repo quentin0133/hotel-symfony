31407d269caf634b9432533132838b7c89d5ccbd
Quentin Yahia <quentin.yahia.pro@gmail.com>
2026-05-23 17:15:49 +0200

    Integration test for chambre :white_check_mark:

--- src/Controller/Admin/ChambreController.php
+++ src/Controller/Admin/ChambreController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/chambre', name: 'admin.chambre.')]
+#[Route('/admin/chambre', name: 'admin.chambre.')]
 final class ChambreController extends AbstractController
 {
     #[Route(name: 'index', methods: ['GET'])]
